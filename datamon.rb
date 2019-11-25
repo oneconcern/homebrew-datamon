fe2b729d5018339c507183234bbb5fdf8866d577
Ritesh H Shukla <ritesh@oneconcern.com>
2019-11-25 14:05:42 -0800

    Debug install

--- datamon.rb
+++ datamon.rb
@@ -4,15 +4,16 @@ class Datamon < Formula
   url "https://github.com/oneconcern/datamon/releases/download/v1.0.0/datamon.mac.tgz"
   version "1.0"
   sha256 "f99844cf4f62034f514c3d06a0d26b97e5e5ac7d26cccf88413e5f0053fc6f06"
-
+  bottle :unneeded
+  
   def install
     # nm = "datamon"
-    tar xzf datamon.mac.tgz
+    # tar xzf datamon.mac.tgz
     #system "mv", "datamon", nm
     bin.install datamon
   end
 
-  test do
-    system "#{bin}/datamon", "version"
-  end
+  # test do
+  #   system "#{bin}/datamon", "version"
+  # end
 end
