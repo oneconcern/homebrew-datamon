72cf2bf7cc692b6cb36be301c44b2557d2138713
Ritesh H Shukla <ritesh@oneconcern.com>
2019-11-25 13:59:53 -0800

    Fix variable

--- datamon.rb
+++ datamon.rb
@@ -6,10 +6,10 @@ class Datamon < Formula
   sha256 "f99844cf4f62034f514c3d06a0d26b97e5e5ac7d26cccf88413e5f0053fc6f06"
 
   def install
-    nm = "datamon"
+    # nm = "datamon"
     tar xzf datamon.mac.tgz
     #system "mv", "datamon", nm
-    bin.install nm
+    bin.install datamon
   end
 
   test do
