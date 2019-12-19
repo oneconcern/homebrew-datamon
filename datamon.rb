65b686858d42efebffcf3255cbfd69375ba8069c
onecrobot <onecrobot@oneconcern.com>
2019-12-20 00:35:09 +0300

    Brew formula update for datamon version v2.0.0-beta

--- datamon.rb
+++ datamon.rb
@@ -7,11 +7,11 @@ class Datamon < Formula
 
   if OS.mac?
     url "https://github.com/oneconcern/datamon/releases/download/v2.0.0-beta/datamon_2.0.0-beta_darwin_amd64.tar.gz"
-    sha256 "24ee3210c8823203ec997c1256066b23e88a0bf7e997d2bb28a842f9bac1c44c"
+    sha256 "04e5a53927942b6b4d11918d19535ed6a0e2620ec746926abde9c1b57ca49dac"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/oneconcern/datamon/releases/download/v2.0.0-beta/datamon_2.0.0-beta_linux_amd64.tar.gz"
-      sha256 "5144919221f28eaf8cc5a57c97eca86b585bf56d52b2dc049a55e6fd0d336c2b"
+      sha256 "3c6b4ea9223dc9a42aef7a2d1f5a682ffa94283290b899b6a66ce24b0e772255"
     end
   end
 
